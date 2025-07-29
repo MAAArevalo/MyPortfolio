65bc3d79b9870960f01becf49bfe1d519ee4c618
Ma Angelica A Arevalo <76656116+MAAArevalo@users.noreply.github.com>
2025-07-30 02:20:46 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -99,10 +99,10 @@ class ReiMain {
                     items:1
                 },
                 600:{
-                    items:3
+                    items:1
                 },
                 1000:{
-                    items:5
+                    items:1
                 }
             }
         })
